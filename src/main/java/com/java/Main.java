b6cf22672c3b40ac6b9dbc275a526781dd9ffba5
Yevhen Tarasenko <yevhen.tarasenko1995@gmail.com>
2026-03-08 21:09:05 +0200

    Lesson 6: if-else, switch-case

--- src/main/java/com/java/Main.java
+++ src/main/java/com/java/Main.java
@@ -66,15 +66,101 @@ import java.util.Scanner;
 //    }
 //}
 
+//class Main {
+//    public static void main(String[] args){
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter first num: ");
+//        int num1 = scan.nextInt();
+//
+//        System.out.print("Enter second num: ");
+//        int num2 = scan.nextInt();
+//
+//        System.out.print("Sum of first and second number: " + (num1 + num2));
+//    }
+//}
+
+
+//    *** lesson 6 ***
+//class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter number: ");
+//        int user = scan.nextInt();
+//        char sym = 't';
+//        if(user == 5 || sym == 't') {
+//            System.out.println("Number is 5");
+//            boolean isHappy = false;
+//            if(!isHappy) {
+//                System.out.println("Bool is false");
+//            }
+//        } else if(user == 60)
+//            System.out.println("Number is 60");
+//        else if(user == 70)
+//            System.out.println("Number is 70");
+//        else if(user > 100)
+//            System.out.println("Number is more 100");
+//        else {
+//            System.out.println("Number is undefined");
+//        }
+//    }
+//}
+
+//class Main {
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter number: ");
+//        int user = scan.nextInt();
+//
+//        switch(user) {
+//            case 5:
+//                System.out.println("Number is 5");
+//                break;
+//            case 50:
+//                System.out.println("Number is 50");
+//                break;
+//            default:
+//                System.out.println("Number is undefined");
+//        }
+//    }
+//}
+
 class Main {
-    public static void main(String[] args){
+    public static void main (String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter first num: ");
-        int num1 = scan.nextInt();
+        System.out.println("Enter number 1: ");
+        int a = scan.nextInt();
 
-        System.out.print("Enter second num: ");
-        int num2 = scan.nextInt();
+        System.out.println("Enter math symbol: ");
+        scan.nextLine();
+        char math = scan.nextLine().charAt(0);
 
-        System.out.print("Sum of first and second number: " + (num1 + num2));
+        System.out.println("Enter number 2: ");
+        int b = scan.nextInt();
+
+        int res = 0;
+        switch (math) {
+            case '+':
+                res = a + b;
+                System.out.println("Res: " + res);
+                break;
+            case '-':
+                res = a - b;
+                System.out.println("Res: " + res);
+                break;
+            case '*':
+                res = a * b;
+                System.out.println("Res: " + res);
+                break;
+            case '/':
+                if(b == 0)
+                    System.out.println("Error");
+                    else {
+                    res = a / b;
+                    System.out.println("Res: " + res);
+                }
+                break;
+            default:
+                System.out.println("Error");
+        }
     }
 }
